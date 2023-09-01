7488331558b97e356c0d1c1a226fece775544653
xianii <xianyi.xia@outlook.com>
2023-09-02 00:33:48 +0800

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import { registerSW } from "virtual:pwa-register"
+
+if ("serviceWorker" in navigator) {
+	registerSW({
+		onOfflineReady() {
+			console.log("offline ready")
+		},
+	})
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
